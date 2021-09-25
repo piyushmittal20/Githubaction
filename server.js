ef3b9872d2c8a7cc762d7a266e10f96d35e18497
piyushmittal20 <20piyushmittal@gmail.com>
2021-09-25 15:32:16 +0530

    testing CI/CD

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ app.use("/", (req, res) => {
   res.send("Hello, I am used for Github Actions!!!!");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello, I am an Hello Route...");
-});
-
 app.listen(4000, () => {
-  console.log(`Server is running on PORT 4000!!!!!`);
+  console.log(`Server is running on PORT 4000!!`);
 });
